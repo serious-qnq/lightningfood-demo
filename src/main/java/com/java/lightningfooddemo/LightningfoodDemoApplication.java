43f273a9876097dd8ec298f6caf327e4ce3cf25e
serious-qnq <810292978@qq.com>
2022-08-10 16:58:56 +0800

    使用sharding-jdbc实现程序的读写分离

--- src/main/java/com/java/lightningfooddemo/LightningfoodDemoApplication.java
+++ src/main/java/com/java/lightningfooddemo/LightningfoodDemoApplication.java
@@ -1,6 +1,8 @@
 package com.java.lightningfooddemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -15,7 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class LightningfoodDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LightningfoodDemoApplication.class, args);
-
         log.info("项目启动成功");
     }
 }
