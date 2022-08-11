dfa68c44df4dc50e92d581c7fe2a723c1eafa917
serious-qnq <810292978@qq.com>
2022-08-11 15:19:44 +0800

    使用swagger生成接口文档方便在线调试

--- src/main/java/com/java/lightningfooddemo/config/WebMvcConfig.java
+++ src/main/java/com/java/lightningfooddemo/config/WebMvcConfig.java
@@ -1,25 +1,38 @@
 package com.java.lightningfooddemo.config;
 
 import com.alibaba.fastjson.support.spring.messaging.MappingFastJsonMessageConverter;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.java.lightningfooddemo.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     //设置静态资源映射
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始静态资源映射");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
@@ -33,4 +46,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //添加到底层
         converters.add(0, messageConverter);
     }
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(aliInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.java.lightningfooddemo.controller"))
+                .paths(PathSelectors.any()).build();
+    }
+
+
+    public ApiInfo aliInfo(){
+        return new ApiInfoBuilder()
+                .title("外卖")
+                .version("1.0")
+                .description("外卖接口文档")
+                .build();
+    }
+
 }
--- src/main/java/com/java/lightningfooddemo/fillter/LoginCheckFilter.java
+++ src/main/java/com/java/lightningfooddemo/fillter/LoginCheckFilter.java
@@ -33,7 +33,8 @@ public class LoginCheckFilter implements Filter {
         String[] urls = new String[]{
                 "/employee/login", "/employee/logout",
                 "/backend/**", "/front/**", "/common/**",
-                "/user/sendMsg", "/user/login"
+                "/user/sendMsg", "/user/login",
+                "/doc.html","/webjars/**","/swagger-resources","/v2/api-docs"
         };
 
         //判断本次请求是否需要处理
